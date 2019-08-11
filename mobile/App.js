b9e742e4d637fb968c9b99a67597a3550f8a37c7
Savio <saviopf22@gmail.com>
2019-08-11 16:41:17 -0300

    hello world da parte mobile do sistema

---
+++ mobile/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native'
+
+
+const App = () => {
+  return (
+    <Fragment>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+})
+
+export default App
