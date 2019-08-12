f177fb77b599444416c56838abbfb6d472095f4a
Savio <saviopf22@gmail.com>
2019-08-12 20:12:04 -0300

    like e dislike funcionando

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
 import {
   Text,
   Image,
@@ -10,100 +10,103 @@ import {
   StatusBar
 } from 'react-native'
 
+import api from '../services/api'
+
 import logo from '../../assets/logo.png'
+
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
 
 function Main({ navigation }) {
 
-  const back = () => {
+  const logged = navigation.getParam('_id')
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function loadUser() {
+      const response = await api.get('/dev', {
+        headers: { user: logged }
+      })
+      setUsers(response.data)
+    }
+    loadUser()
+  }, [logged])
+
+  async function handleLike() {
+    //const firstUser = users[0]
+    const [firstUser, ...rest] = users
+
+    await api.post(`/dev/${firstUser._id}/likes`, null, {
+      headers: { user: logged }
+    })
+
+    setUsers(rest)
+  }
+
+  async function handleDislike() {
+    const [firstUser, ...rest] = users
+
+    await api.post(`/dev/${firstUser._id}/dislikes`, null, {
+      headers: { user: logged }
+    })
+    setUsers(rest)
+  }
+
+  async function logout() {
+    await AsyncStorage.clear()
+
     navigation.navigate('Login')
   }
 
   return (
-    <Fragment>
+
+    <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor='#f5f5f5' barStyle='dark-content' />
-      <View>
-        {/* test api */}
-        <Text>{navigation.getParam('user')}</Text>
-      </View>
-      <SafeAreaView style={styles.container}>
-        <Image source={logo} />
-        <View style={styles.cardContainer}>
-          <View style={styles.card}>
+      <TouchableOpacity onPress={logout}>
+        <Image style={styles.logo} source={logo} />
+      </TouchableOpacity>
+
+      <View style={styles.cardContainer}>
+        {(users.length > 0) ? (users.map((user, i) => (
+          <View key={user._id} style={[styles.card, { zIndex: users.length - i }]}>
             <Image
               style={styles.avatar}
-              source={{ uri: 'https://avatars1.githubusercontent.com/u/19521709?v=4' }}
+              source={{ uri: user.avatar }}
             />
             <View style={styles.footer}>
-              <Text style={styles.name}>Linus</Text>
+              <Text style={styles.name}>{user.name}</Text>
               <Text
                 numberOfLines={3}
-                style={styles.bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in tincidunt felis. Maecenas consequat gravida orci, ac tempus augue elementum vel. Donec varius risus et tellus semper, non auctor est porta. Sed maximus nibh tincidunt ipsum gravida, ut ornare risus porttitor.
+                style={styles.bio}
+              >
+                {user.bio}
               </Text>
             </View>
           </View>
+        ))) : <Text style={styles.empty}>Acabou:(</Text>}
+      </View>
 
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleLike} >
+          <Image source={like} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleDislike}>
+          <Image source={dislike} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
 
-          {/* tests css */}
-          <View style={styles.card}>
-            <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/1024025?v=4' }} />
-            <View style={styles.footer}>
-              <Text style={styles.name}>Linus</Text>
-              <Text
-                numberOfLines={3}
-                style={styles.bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in tincidunt felis. Maecenas consequat gravida orci, ac tempus augue elementum vel. Donec varius risus et tellus semper, non auctor est porta. Sed maximus nibh tincidunt ipsum gravida, ut ornare risus porttitor.
-              </Text>
-            </View>
-          </View>
-
-          <View style={styles.card}>
-            <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/1024025?v=4' }} />
-            <View style={styles.footer}>
-              <Text style={styles.name}>Linus</Text>
-              <Text
-                numberOfLines={3}
-                style={styles.bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in tincidunt felis. Maecenas consequat gravida orci, ac tempus augue elementum vel. Donec varius risus et tellus semper, non auctor est porta. Sed maximus nibh tincidunt ipsum gravida, ut ornare risus porttitor.
-              </Text>
-            </View>
-          </View>
-        </View>
-        <View style={styles.card}>
-          <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/1024025?v=4' }} />
-          <View style={styles.footer}>
-            <Text style={styles.name}>Linus</Text>
-            <Text
-              numberOfLines={3}
-              style={styles.bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in tincidunt felis. Maecenas consequat gravida orci, ac tempus augue elementum vel. Donec varius risus et tellus semper, non auctor est porta. Sed maximus nibh tincidunt ipsum gravida, ut ornare risus porttitor.
-              </Text>
-          </View>
-        </View>
-        <View style={styles.card}>
-          <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/1024025?v=4' }} />
-          <View style={styles.footer}>
-            <Text style={styles.name}>Linus</Text>
-            <Text
-              numberOfLines={3}
-              style={styles.bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in tincidunt felis. Maecenas consequat gravida orci, ac tempus augue elementum vel. Donec varius risus et tellus semper, non auctor est porta. Sed maximus nibh tincidunt ipsum gravida, ut ornare risus porttitor.
-              </Text>
-          </View>
-        </View>
-        <View style={styles.card}>
-          <Image style={styles.avatar}
-            source={{ uri: 'https://avatars0.githubusercontent.com/u/1024025?v=4' }} />
-          <View style={styles.footer}>
-            <Text style={styles.name}>Linus</Text>
-            <Text
-              numberOfLines={3}
-              style={styles.bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in tincidunt felis. Maecenas consequat gravida orci, ac tempus augue elementum vel. Donec varius risus et tellus semper, non auctor est porta. Sed maximus nibh tincidunt ipsum gravida, ut ornare risus porttitor.
-              </Text>
-          </View>
-        </View>
-
-      </SafeAreaView >
-    </Fragment >
   )
 }
 
 const styles = StyleSheet.create({
+  empty: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    color: '#999',
+    fontSize: 26,
+    fontWeight: 'bold'
+  },
   container: {
     flex: 1,
     alignItems: 'center',
@@ -135,7 +138,7 @@ const styles = StyleSheet.create({
   },
   avatar: {
     flex: 1,
-    height: 100,
+    height: 300,
   },
   name: {
     fontSize: 16,
@@ -146,7 +149,24 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#999',
     marginTop: 5,
-    lineHeight: 20
+    lineHeight: 18
+  },
+  logo: {
+    marginTop: 30
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginBottom: 30,
+  },
+  button: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    elevation: 2,
   }
 })
 
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
-const myUrl = '000.000.0.000'
+//const myUrl = '000.000.0.000'
 //const myUrl = 'localhost'
+
+const myUrl = '192.168.0.111'
 
 const api = axios.create({
     baseURL: `http://${myUrl}:7777`
